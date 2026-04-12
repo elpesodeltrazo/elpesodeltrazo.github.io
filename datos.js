d05e2e37a9fe967264c97b609584ec019ec13948
elpesodeltrazo <elpesodeltrazo@gmail.com>
2026-04-12 19:34:09 +0200

    Update datos.js

--- datos.js
+++ datos.js
@@ -13,7 +13,7 @@ const RESENAS = [
     pais: 'España',
     pais_key: 'españa',
     imagen: 'img/arrugas.jpg',
-    imagen_posicion: 'center 50%',
+    imagen_posicion: 'center 45%',
     vinetas: [
       'img/arrugas-vineta.jpg',
       // 'img/arrugas-vineta2.jpg'
